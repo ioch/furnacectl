43698556fde27c6f624c9205bbb74d9910a76108
miceuz <mic@hardcore.lt>
2017-04-27 12:29:55 +0300

    Add temperature profile running library

---
+++ termocouple_test/ProgramProfile.cpp
@@ -0,0 +1,62 @@
+#include "ProgramProfile.h"
+
+uint8_t ProgramProfile::programShouldAdvance() {
+  return !programIsDone &&
+         (PHASE_SOAK == programStepPhase && 
+          (millis() - stepStartTs) / 60000 > program[currentStep].minutesAtTarget);
+}
+
+void ProgramProfile::stepInit(uint16_t currTemp) {
+  stepStartTemperature = currTemp;
+  programStepPhase = PHASE_RAMP;
+  stepStartTs = millis();
+}
+
+uint8_t ProgramProfile::isMoreStepsLeft() {
+  return currentStep + 1 < programStepsTotal;
+}
+
+uint8_t ProgramProfile::isTimeToRamp() {
+  return PHASE_RAMP == programStepPhase && 
+         millis() - lastProgramInvocationTs > PROGRAM_IMVOCATION_INTERVAL;
+}
+
+uint16_t ProgramProfile::computeSetpoint() {
+  return stepStartTemperature + (millis() - stepStartTs) / 60000 * program[currentStep].rampDegreesPerMinute;
+}
+
+uint8_t ProgramProfile::isTargetSetpointReached(uint16_t currTemp) {
+  return currTemp >= program[currentStep].targetTemp;
+}
+
+void ProgramProfile::programStart(uint16_t currTemp) {
+  currentStep = 0;
+  stepInit(currTemp);
+}
+
+void ProgramProfile::programCompute(uint16_t currTemp) {
+  if(programShouldAdvance()) {
+    if(isMoreStepsLeft()) {
+      currentStep = currentStep + 1;
+      stepInit(currTemp);
+    } else {
+      programIsDone = 1;
+      programDoneCallback();
+    }
+  } else if(isTimeToRamp()){
+    if(isTargetSetpointReached(currTemp)) {
+      programStepPhase = PHASE_SOAK;
+      stepStartTs = millis();
+    } else {
+      uint16_t newSetpoint = computeSetpoint();
+      setpointCallback(newSetpoint);
+    }
+  } 
+  lastProgramInvocationTs = millis();
+}
+
+/**
+5°/h iki 120 
+prie 120 30min
+išjungti
+**/
---
+++ termocouple_test/ProgramProfile.h
@@ -0,0 +1,55 @@
+#ifndef __PROGRAM_PROFILE
+#define __PROGRAM_PROFILE
+
+#if (ARDUINO < 100)
+#include "WProgram.h"
+#else
+#include "Arduino.h"
+#endif
+
+#define STEP_NONE 255
+#define PROGRAM_IMVOCATION_INTERVAL 60000
+
+typedef enum{
+  PHASE_RAMP,
+  PHASE_SOAK
+} ProgramStepPhase_t;
+
+typedef struct{
+  float rampDegreesPerMinute;
+  uint16_t targetTemp;
+  uint16_t minutesAtTarget;
+} ProgamStep_t;
+
+class ProgramProfile
+{
+public:
+	ProgramProfile(void (*spCallback)(uint16_t), void (*pdCallback) (void)) {
+		setpointCallback = spCallback;
+		programDoneCallback = pdCallback;
+	}
+	void programStart(uint16_t currTemp);
+	void programCompute(uint16_t currTemp);
+
+private:
+	void (*setpointCallback)(uint16_t);
+	void (*programDoneCallback) (void);
+	uint16_t stepStartTemperature = 0;
+	ProgramStepPhase_t programStepPhase = PHASE_RAMP;
+	uint8_t currentStep = STEP_NONE;
+
+	uint32_t lastProgramInvocationTs = 0;
+	uint32_t stepStartTs = 0;
+	uint8_t programIsDone = 0;
+	ProgamStep_t program[1] = {{0.83, 120, 30}};
+	uint8_t programStepsTotal = sizeof(program) / sizeof(program[0]);
+
+	uint8_t programShouldAdvance();
+	void stepInit(uint16_t currTemp);
+	uint8_t isMoreStepsLeft();
+	uint8_t isTimeToRamp();
+	uint16_t computeSetpoint();
+	uint8_t isTargetSetpointReached(uint16_t currTemp);
+};
+
+#endif
